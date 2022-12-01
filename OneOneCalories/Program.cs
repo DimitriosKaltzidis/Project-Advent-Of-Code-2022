99ec6191df7dcc9eee19a6f103fcaa6a49074e6f
Dimitrios Kaltzidis <dimitrios.kaltzidis@mail.com>
2022-12-01 22:00:06 +0200

    Add project files.

---
+++ OneOneCalories/Program.cs
@@ -0,0 +1,37 @@
+﻿var elves = new List<int>();
+string[] lines = File.ReadAllLines(@"dayone.txt");
+
+var elfCalorieCounter = 0;
+
+foreach (string line in lines)
+{
+    if(line == string.Empty)
+    {
+        elves.Add(elfCalorieCounter);
+        elfCalorieCounter = 0;
+    }
+    else
+    {
+        elfCalorieCounter += Int32.Parse(line);
+    }
+}
+
+var caloriesOfMostPreparedElf = elves.Max();
+var indexOfMostPreparedElf = elves.IndexOf(caloriesOfMostPreparedElf);
+
+Console.WriteLine($"Most Prepared Elf");
+Console.WriteLine($"Calories: {caloriesOfMostPreparedElf}, Position:{indexOfMostPreparedElf}");
+
+var sortedElves = elves.OrderByDescending(elve => elve).ToList();
+
+var topThreeElves = sortedElves.Take(3).ToList();
+
+Console.WriteLine($"Top three prepared elves");
+foreach (var elve in topThreeElves)
+{
+    Console.WriteLine($"Calories: {elve}");
+}
+
+Console.WriteLine($"Total calories: {topThreeElves.Sum(elf => elf)}");
+
+Console.ReadLine();
